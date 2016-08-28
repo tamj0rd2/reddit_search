c61121b3204871611e4573cea0d6ea8f64e2068a
hamzicabdulah <abdulahhamzic@gmail.com>
2016-08-28 16:49:14 +0200

    popup: Added search function on pressing enter

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,29 +1,32 @@
 $(document).ready(function(){
+  
   var removeWarn = function(){
     $('#search_query').removeClass('warning');
     $('small').hide();    
   };
-  $('a').on('click', function(){
+
+  $('button').on('click', function(){
     if(!$('#search_query').val()){
       $('#search_query').addClass('warning');
-      $('small').fadeIn(100);
+      $('small').show();
       return false;
     }
     else if (!$('#subreddit').val() && !$('#google_domain').val()){
-      $('#searchButton').attr('href', 'https://www.google.com/#q=site:www.reddit.com ' + $('#search_query').val());        
+      window.location.href = 'https://www.google.com/#q=site:www.reddit.com ' + $('#search_query').val();        
       removeWarn();       
     }
     else if (!$('#google_domain').val()){
-      $('#searchButton').attr('href', 'https://www.google.com/#q=site:www.reddit.com%2F' + $('#subreddit').val() + '+' + $('#search_query').val());        
+      window.location.href = 'https://www.google.com/#q=site:www.reddit.com%2F' + $('#subreddit').val() + '+' + $('#search_query').val();        
       removeWarn();          
     }
     else if (!$('#subreddit').val()){
-      $('#searchButton').attr('href', 'https://www.' + $('#google_domain').val() + '/#q=site:www.reddit.com ' + $('#search_query').val());          
+      window.location.href = 'https://www.' + $('#google_domain').val() + '/#q=site:www.reddit.com ' + $('#search_query').val();          
       removeWarn();   
     }
     else{
-      $('#searchButton').attr('href', 'https://www.' + $('#google_domain').val() + '/#q=site:www.reddit.com%2F' + $('#subreddit').val() + '+' + $('#search_query').val());
+      window.location.href = 'https://www.' + $('#google_domain').val() + '/#q=site:www.reddit.com%2F' + $('#subreddit').val() + '+' + $('#search_query').val();
       removeWarn();        
     }
-  });
+  })
+  
 });
