95adeb0bc25e290613f1c0b5b90f434e92faeaaf
hamzicabdulah <abdulahhamzic@gmail.com>
2016-08-29 15:41:34 +0200

    Replaced 'warning' class with Bootstrap 'has-error' class.

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
   
+  //Declaring a function to be used when the 'invalid form' warning should be removed.
   var removeWarn = function(){
-    $('#search_query').removeClass('warning');
+    $('.query_group').removeClass('has-error');
     $('small').hide();    
   };
 
   $('button').on('click', function(){
+    //The user should see a warning if they leave the search query are empty.
     if(!$('#search_query').val()){
-      $('#search_query').addClass('warning');
+      $('.query_group').addClass('has-error');
       $('small').show();
       return false;
     }
+    //When the user leaves out the google domain area and the subreddit area empty, 'google.com' should be the 
     else if (!$('#subreddit').val() && !$('#google_domain').val()){
       window.location.href = 'https://www.google.com/#q=site:www.reddit.com ' + $('#search_query').val();        
       removeWarn();       
