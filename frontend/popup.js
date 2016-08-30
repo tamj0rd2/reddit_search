41a5662bf9b151567620f22c77805052acc6545e
Sawan <sawan.dontgiveadamn@gmail.com>
2016-08-30 13:23:54 +0530

    popup.js: added and functionality to autocomplete subreddit search input box added

---
+++ frontend/popup.js
@@ -0,0 +1,45 @@
+//file to implement autocomplete functionality
+
+  console.log("working");
+    
+  //Adding autocomplete to subreddit search box
+
+  //get the value in the search box
+  $("#subreddit").keyup(function() {
+    console.log("keyup working");
+    var val = $("#subreddit").val();
+    if(val > 3){
+      $.ajax({
+        url: "https://www.reddit.com/search.json?q="+val+"&type=sr",
+        dataType: "json",
+        type: "GET",
+        complete: function(){
+          console.log("complete function being executed");
+        },
+        error: function(){
+          console.log("error fetching data");
+        },
+        success: function(json){
+          //console.log(JSON.stringify(json, "", 4));
+          //console.log(json.data.children.length);
+          var sub = json.data.children;
+          for(var i= 0; i< sub.length; i++){
+            if(typeof sub[i] === "object"){
+              for(var key in sub[i]){
+                if(sub[i].hasOwnProperty(key)){
+                  if(key === "data"){
+                    for(var k in sub[i][key]){
+                      if(k === "display_name"){
+                        $("#sub_feed").append("<option value='"+sub[i][key][k]+"'>");
+                        //console.log("\n"+"<option value='"+sub[i][key][k]+"'>"+"\n");
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }  
+      });
+    }
+  });
