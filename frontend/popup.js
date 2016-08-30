1c224c44b973bcdb5643ad5e56dc8a475b64926c
hamzicabdulah <abdulahhamzic@gmail.com>
2016-08-30 20:17:10 +0200

    popup.js: Fixed bug and url now opens in a new tab The Google search now opens in a new tab on search button click

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-  
+
+  var url, win;
+
+  var focusFunction = function(){
+      //Declaring a function that will be called on search button click — it opens the url in a new tab and switches over to it
+      win = window.open(url, '_blank');
+      win.focus();
+  }
+
   var removeWarn = function(){
     //Declaring a function to be used when the 'invalid form' warning should be removed.
     $('.query_group').removeClass('has-error');
-    $('small').hide();    
+    $('small').hide();
   };
 
   $('button').on('click', function(){
@@ -11,20 +19,20 @@ $(document).ready(function(){
     if(!$('#search_query').val()){
       $('.query_group').addClass('has-error');
       $('small').show();
-      return false;
     }
-    //When the user leaves out the subreddit area, the Google search will allow the user not to use one
+    //When the user leaves out the subreddit area, the Google search will allow the user not to use one.
     else if (!$('#subreddit').val()){
-      window.location.href = 'https://www.google.com' + '/#q=site:www.reddit.com ' + $('#search_query').val();          
-      removeWarn();   
+      url = 'https://www.google.com' + '/#q=site:www.reddit.com ' + $('#search_query').val();
+      focusFunction();
+      removeWarn();
     }
-    //When both areas are filled in, Google searches through a specific subreddit with a specific query
+    //When both areas are filled in, Google searches through a specific subreddit with a specific query.
     else{
-      window.location.href = 'https://www.google.com' + '/#q=site:www.reddit.com%2F' + $('#subreddit').val() + '+' + $('#search_query').val();
-      removeWarn();        
+      url = 'https://www.google.com' + '/#q=site:www.reddit.com%2F' + $('#subreddit').val() + '+' + $('#search_query').val();
+      focusFunction();
+      removeWarn();
     }
+    return false;
   });
-  
+
 });
-
-
