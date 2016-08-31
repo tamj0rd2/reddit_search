4b46e2ee5c423a5952d3a7052df6bd5882dbc0d1
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-08-31 14:50:56 +0100

    popup.js: Make function stubs
    To be expanded on later.
    
    Improves: https://github.com/tamj0rd2/reddit_search/issues/5

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,2 +1,18 @@
 /*jslint browser: true indent:2 maxlen: 80*/
 /*global $, jQuery, alert, console*/
+
+$(document).ready(function () {
+  'use strict';
+
+  function createSearchUrl() {
+
+  }
+
+  function search() {
+
+  }
+
+  $('button').click(function () {
+
+  });
+});
