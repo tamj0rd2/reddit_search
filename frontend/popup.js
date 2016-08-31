33ee45e52bf8e06f0f8d286fc9c06edac40a6bb6
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-08-31 15:10:13 +0100

    popup.js: Flesh out createSearchUrl()
    Define subreddit as a var so that it doesn't need to be evaluated twice.
    
    Improves https://github.com/tamj0rd2/reddit_search/issues/5

--- frontend/popup.js
+++ frontend/popup.js
@@ -5,7 +5,15 @@ $(document).ready(function () {
   'use strict';
 
   function createSearchUrl() {
+    var url, subreddit;
+    url = 'https://www.google.com/search?q=site:www.reddit.com/';
+    subreddit = $('#subreddit').val();
 
+    if (subreddit) {
+      url += 'r/' + subreddit;
+    }
+    url += '+' + $('#search_query').val();
+    return url;
   }
 
   function search() {
