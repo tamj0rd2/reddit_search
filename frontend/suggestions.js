960af211c9b5d30c9c92a901c2354c7675f46c47
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-10-08 10:35:59 +0100

    suggestions: Load suggestions on valid input
    Suggestions will only be reloaded if an alphanumeric character
    or an underscore is typed in.

--- frontend/suggestions.js
+++ frontend/suggestions.js
@@ -46,13 +46,22 @@ $(document).ready(function () {
     setSuggestions(suggestions);
   }
 
-  function validateInput() {
+  function inputIsValid() {
+    // returns true if the subreddit is only alphanumeric with underscores
+    if (/[a-zA-Z0-9_]+$/g.test($('#subreddit').val())) {
+      return true;
+    }
+    // returns false if the user has typed in a special character and also
+    // removes all special characters
     var newVal = $('#subreddit').val().replace(/[^a-zA-Z0-9_]+/g, '');
     $('#subreddit').val(newVal);
+    return false;
   }
 
   $('#subreddit').on('input', function getSubs() {
-    validateInput();
-    chrome.storage.local.get('subs', showSuggestions);
+    // don't reload the suggestions if the user enters invalid input
+    if (inputIsValid()) {
+      chrome.storage.local.get('subs', showSuggestions);
+    }
   });
 });
