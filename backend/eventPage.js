b492c0ef464c5f62312e9d82338e5b729c783ea5
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-10-11 12:30:47 +0100

    eventPage.js: Refactor functions
    Move listeners into self executing function so that future listeners
    can share common functions and variables.
    
    Closes https://github.com/tamj0rd2/reddit_search/issues/29

--- backend/eventPage.js
+++ backend/eventPage.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function (details) {
+(function () {
   'use strict';
 
   function caseInsensitiveSort(array) {
@@ -9,41 +9,33 @@ chrome.runtime.onInstalled.addListener(function (details) {
     });
   }
 
-  function initDefaultTab() {
-    chrome.storage.local.set({'defaultTab': 'new_tab'});
-  }
-
-  function initSubs(subs) {
-    subs = caseInsensitiveSort(subs);
-    chrome.storage.local.set({'subs': subs});
-  }
-
-  if (details.reason === 'install') {
-    /* Initialises the chrome storage for the extension */
-    $.getJSON('backend/topsubreddits.json', initSubs);
-    initDefaultTab();
-  }
-});
-
-chrome.runtime.onMessage.addListener(
-  function (message) {
-    'use strict';
-
-    function caseInsensitiveSort(array) {
-      /* Sort an array, ignoring the case of the items */
-      return array.sort(function (a, b) {
-        var atemp = a.toLowerCase(), btemp = b.toLowerCase();
-        return atemp > btemp ? 1 : atemp < btemp ? -1 : 0;
-      });
+  chrome.runtime.onInstalled.addListener(function (details) {
+    function initDefaultTab() {
+      chrome.storage.local.set({'defaultTab': 'new_tab'});
     }
 
-    if (message.msgType === 'add_sub') {
-      chrome.storage.local.get('subs', function addASub(result) {
-        var subs = result.subs;
-        subs.push(message.sub);
-        subs = caseInsensitiveSort(subs);
-        chrome.storage.local.set({'subs': subs});
-      });
+    function initSubs(subs) {
+      subs = caseInsensitiveSort(subs);
+      chrome.storage.local.set({'subs': subs});
     }
-  }
-);
+
+    if (details.reason === 'install') {
+      /* Initialises the chrome storage for the extension */
+      $.getJSON('backend/topsubreddits.json', initSubs);
+      initDefaultTab();
+    }
+  });
+
+  chrome.runtime.onMessage.addListener(
+    function (message) {
+      if (message.msgType === 'add_sub') {
+        chrome.storage.local.get('subs', function addASub(result) {
+          var subs = result.subs;
+          subs.push(message.sub);
+          subs = caseInsensitiveSort(subs);
+          chrome.storage.local.set({'subs': subs});
+        });
+      }
+    }
+  );
+}());
